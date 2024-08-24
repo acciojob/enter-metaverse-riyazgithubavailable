80fb5656be760e6d3459fb31cb2f20a8abf93223
riyazgithubavailable <144905521+riyazgithubavailable@users.noreply.github.com>
2024-08-24 21:49:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 //your JS code here. If required.
+// const para = document.getElementById("status");
+// const button = document.getElementById("enterBtn")
+// const body = document.body;
+// button.addEventListener("click",()=>{
+// 	const head = document.createElement("h1");
+// 	head.id="status"
+// 	head.innerText="Entered Metaverse"
+// 	body.insertBefore(head,para);
+// 	para.remove()
+// })
 const para = document.getElementById("status");
-const button = document.getElementById("enterBtn")
-const body = document.body;
-button.addEventListener("click",()=>{
-	const head = document.createElement("h1");
-	head.id="status"
-	head.innerText="Entered Metaverse"
-	body.insertBefore(head,para);
-	para.remove()
-})
+const button = document.getElementById("enterBtn");
+
+button.addEventListener("click", () => {
+  const heading = document.createElement("h1");
+  heading.innerText   
+ = "Entered Metaverse";
+
+  // Replace the paragraph with the heading
+  para.parentNode.replaceChild(heading, para);
+});
